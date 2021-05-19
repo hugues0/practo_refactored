80deb6a681f5915008c3e40e7ea6885f76bda36c
hugues0 <ntwari.hugues@gmail.com>
2021-05-19 23:53:35 +0200

    user validation middlewar refactored

--- middlewares/usersvalidation.js
+++ middlewares/usersvalidation.js
@@ -1,10 +1,15 @@
 const Joi = require('joi');
+const response = require('../helpers/response');
 
-module.exports = function validateUser(user){
+module.exports = function validateUser(req,res,next){
      const schema = {
         username: Joi.string().required().email(),
         password: Joi.string().required().min(5),
     };
-    return Joi.validate(user, schema);
+    //return Joi.validate(user, schema);
     
-    }
+    const {error} = Joi.validate(req.body,schema);
+    if (error) return response.response(res,422,'error',`${error.details[0].message}`,true);
+    next();
+
+    };
--- routes/usersroute.js
+++ routes/usersroute.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const userscontroller = require ('../controllers/userscontroller')
-
+const validateUser = require('../middlewares/usersvalidation');
 const { addUser, loginUser } = userscontroller;
 
 const router = express.Router();
 
-router.post('/auth/signup',addUser );
+router.post('/auth/signup',validateUser,addUser );
 router.post('/auth/signin',loginUser);
 
 module.exports = router;
